221c85174e981a084ea15dd03a958038f7e3f022
SeungWan <tmddhks0104@gmail.com>
2020-09-21 17:16:04 +0900

    Updates

---
+++ precache-manifest.8700e5cf7a107fea57a86375a894525d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40d4d6164de6b4a24312c3668201a51c",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "b4cac1e8fdb9bfbaeabf",
+    "url": "/nwitter/static/css/main.9333dc4c.chunk.css"
+  },
+  {
+    "revision": "73dbbc2f21c4a7068314",
+    "url": "/nwitter/static/js/2.84fa4449.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/nwitter/static/js/2.84fa4449.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4cac1e8fdb9bfbaeabf",
+    "url": "/nwitter/static/js/main.a8a45fa3.chunk.js"
+  },
+  {
+    "revision": "97fb235846154051176c",
+    "url": "/nwitter/static/js/runtime-main.222ae764.js"
+  }
+]);
